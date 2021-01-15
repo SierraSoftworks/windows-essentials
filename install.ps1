e9311cbdc3a7d56f3e1f5541d479bbf3d54b4e44
Benjamin Pannell <benjamin@pannell.dev>
2021-01-15 20:51:10 +0000

    tweak: Improvements to the install process to configure your local shell environment

--- install.ps1
+++ install.ps1
@@ -1,7 +1,18 @@
 # This script uses the winget command provided by the Windows Package Manager.
 # You can read more about it here: https://devblogs.microsoft.com/commandline/windows-package-manager-preview/
 
-if ((Get-Command winget -ErrorAction SilentlyContinue) -eq $null) {
+param(
+    # Specify the directory into which command line tools and other $PATH-available user utilities will be installed.
+    [Parameter(Mandatory = $false)]
+    [string]
+    $UserProgramFolder = "${HOME}\Programs",
+
+    [Parameter(Mandatory = $false)]
+    [string]
+    $DevelopmentDirectory = "${HOME}\dev"
+)
+
+if ($null -eq (Get-Command winget -ErrorAction SilentlyContinue)) {
     Write-Error "You do not have the Windows Package Manager installed on your machine. Please install it before proceeding." `
                 -Category NotInstalled `
                 -RecommendedAction "Install the Windows Package Manager using the steps on this page: https://devblogs.microsoft.com/commandline/windows-package-manager-preview/"
@@ -11,22 +22,27 @@ if ((Get-Command winget -ErrorAction SilentlyContinue) -eq $null) {
 
 Write-Host Installing browser
 winget install Microsoft.Edge
+winget install Mozilla.Firefox
 
 Write-Host Installing communication tools
 winget install keybase
 winget install telegram
 winget install whatsapp
 winget install Microsoft.Teams
+winget install Signal.Signal
 
 Write-Host Installing common utilities
 winget install 7zip
 winget install Microsoft.Onedrive
 winget install 1Password
+winget install calibre.calibre
 
 Write-Host Installing developer environment tools
 winget install git
 winget install gnupg.Gpg4win
 winget install Axosoft.GitKraken
+winget install GitHub.GitHubDesktop
+winget install GitHub.cli
 winget install Docker.DockerDesktop
 winget install python
 winget install node
@@ -43,8 +59,101 @@ winget install Valve.Steam
 winget install OBSProject.OBSStudio
 winget install TeamSpeakSystems.TeamSpeakClient
 
+
 Write-Host Configure PowerShell
-Set-ExecutionPolicy Unrestricted
-$SetupScript = Invoke-WebRequest https://gist.githubusercontent.com/SPARTAN563/77ccb460948afd826365e85d226509a7/raw/setup.ps1
-$ScriptBlock = [ScriptBlock]::Create($SetupScript.Content)
-Invoke-Command -ScriptBlock $ScriptBlock
+$env:PATH += ";${UserProgramFolder}"
+[Environment]::SetEnvironmentVariable("Path", $env:PATH, [System.EnvironmentVariableTarget]::User)
+
+Install-PoshGit
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+
+if (Test-Path -PathType Leaf -Path $profile.CurrentUserAllHosts) {
+    Write-Warning "You currently have a PowerShell profile file, we will not override it."
+} else {
+    Write-Host "Updating your PowerShell Profile file"
+    Get-Content [System.IO.Path]::Combine($PSScriptRoot, "profile.ps1") | Set-Content $profile.CurrentUserAllHosts
+}
+
+
+if (-not (Get-Command "git-tool.exe" -ErrorAction SilentlyContinue)) {
+    Write-Host "Installing Git-Tool"
+    Install-GitHubBinary -Repo "SierraSoftworks/Git-Tool" -File "git-tool-windows-amd64.exe" -Destination "${UserProgramFolder}\git-tool.exe"
+
+    New-Item -ItemType Directory -Path $DevelopmentDirectory
+    
+    if ($null -eq $env:GITTOOL_CONFIG) {
+        $env:GITTOOL_CONFIG = "$DevelopmentDirectory\git-tool.yml"
+        [Environment]::SetEnvironmentVariable("GITTOOL_CONFIG", $env:GITTOOL_CONFIG, [System.EnvironmentVariableTarget]::User)
+    }
+
+    if (-not (Test-Path -PathType Leaf -Path $env:GITTOOL_CONFIG)) {
+        &"git-tool.exe" config | Set-Content "$env:GITTOOL_CONFIG"
+    }
+
+} else {
+    Write-Host "Updating Git-Tool to the latest version"
+    git-tool.exe update
+}
+
+if (-not (Get-Command "starship.exe" -ErrorAction SilentlyContinue)) {
+    Write-Host "Installing Starship"
+    Install-GitHubBinary -Repo "starship/starship" -File "starship-x86_64-pc-windows-msvc.zip" -Destination "${HOME}\Downloads\starship.zip"
+    Expand-Archive -Path "${HOME}\Downloads\starship.zip" -DestinationPath $UserProgramFolder
+}
+
+function Install-GitHubBinary {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Repo,
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $File,
+
+        [Parameter(Mandatory = $false)]
+        [string]
+        $Destination = $null,
+
+        [Parameter(Mandatory = $false)]
+        [string]
+        $ReleaseTag = "latest"
+    )
+
+    if ($ReleaseTag -eq "latest") {
+        $ReleaseTag = (Invoke-WebRequest "https://api.github.com/repos/${Repo}/releases" | ConvertFrom-Json)[0].tag_name
+    }
+
+    if ($Destination -eq $null) {
+        $Destination = $File
+    }
+
+    $DestinationDir = [System.IO.Directory]::GetParent($Destination)
+    if (-not $DestinationDir.Exists) {
+        $DestinationDir.Create()
+    }
+
+    Write-Host "Installing ${Repo}:${File}@${ReleaseTag} into ${Destination}"
+    Invoke-WebRequest -Uri "https://github.com/${Repo}/releases/download/${ReleaseTag}/${File}" -OutFile $Destination
+}
+
+function Install-PoshGit {
+    param (
+        [Parameter(Mandatory = $false)]
+        [string]
+        $Version = "latest"
+    )
+
+    if (-not (Get-Module -ListAvailable -Name PowerShellGet)) {
+        # Install the PowerShellGet module for the current user
+        Install-Module PowerShellGet -Scope CurrentUser -Force -AllowClobber
+    }
+
+    if ($Version -eq "latest") {
+        $Version = (PowerShellGet\Find-Module -AllVersions -AllowPrerelease posh-git | Select-Object -First 1).Version
+        Write-Host "Latest version of PoshGit: $Version"
+    }
+
+    Write-Host "Installing Posh-Git@$Version"
+    PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force -RequiredVersion $Version
+}
---
+++ profile.ps1
@@ -0,0 +1,6 @@
+Import-Module posh-git
+
+Invoke-Expression $(git-tool.exe shell-init powershell)
+New-Alias -Name gt -Value git-tool.exe
+
+Invoke-Expression (&starship init powershell)
